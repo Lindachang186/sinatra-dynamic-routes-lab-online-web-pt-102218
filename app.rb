a7f3b080b9046957e7aece6d6d19cadeffea8f2f
Linda Chang <lindachang186@gmail.com>
2019-02-08 17:25:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require 'pry'
+
 class App < Sinatra::Base
   # Write your code here!
 
@@ -10,6 +11,16 @@ class App < Sinatra::Base
   get '/square/:number' do
     @number = params[:number]
     "#{@number.to_i * @number.to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    x = []
+    "#{@number.times do
+        x << @phrase
+    end}"
+    x.flatten.to_s
   end
 
 end
