df89d65813834229de195f5c3ab5fafed6e9cd32
Linda Chang <lindachang186@gmail.com>
2019-02-08 17:36:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -27,4 +27,19 @@ class App < Sinatra::Base
     @sentence = "#{params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4]+ " " + params[:word5] + "."}"
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    "#{if @operation == "add"
+      @number1 + @number2
+    elsif @operation == "subtract"
+      @number1 - @number2
+    elsif @operation == "multiply"
+      @number1 * @number2
+    elsif @operation == "divide"
+      @number1 / @number2
+    end}"
+  end
+
 end
